69f148eccc59791ba082e912afc72c3667c38395
nemanj4t <nemanja.turanjanin@elfak.rs>
2019-01-26 14:05:30 +0100

    Store setup + shoping cart + wish list + setup products/product.vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+import Vuex from 'vuex';
+import StoreData from './store';
 
 require('./bootstrap');
 
@@ -21,16 +23,20 @@ window.Vue = require('vue');
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
+Vue.use(Vuex);
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('categories', require('./components/CategoriesComponent.vue').default);
+Vue.component('shopping-cart', require('./components/ShoppingCart.vue').default);
+Vue.component('wish-list', require('./components/WishList.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+const store = new Vuex.Store(StoreData);
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    store
 });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,47 +114,11 @@
                         <div class="col-md-3 clearfix">
                             <div class="header-ctn">
                                 <!-- Wishlist -->
-                                <div>
-                                    <a href="#">
-                                        <i class="fas fa-heart"></i>
-                                        <span>Your Wishlist</span>
-                                        <div class="qty">2</div>
-                                    </a>
-                                </div>
+                                <wish-list></wish-list>
                                 <!-- /Wishlist -->
 
                                 <!-- Cart -->
-                                <div class="dropdown">
-                                    <a href="#" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fas fa-shopping-cart"></i>
-                                        <span>Your Cart</span>
-                                        <div class="qty">3</div>
-                                    </a>
-                                    <div class="dropdown-menu p-4" aria-labelledby="dropdownMenuLink">
-                                        <div class="cart-list">
-                                            <div class="product-widget">
-                                                <div class="product-img">
-                                                    <img src="http://www.independentmediators.co.uk/wp-content/uploads/2016/02/placeholder-image.jpg" alt="">
-                                                </div>
-                                                <div class="product-body">
-                                                    <h3 class="product-name"><a href="#">product name goes here</a></h3>
-                                                    <h4 class="product-price"><span class="qty">1x</span>$980.00</h4>
-                                                </div>
-                                                <button class="delete"><i class="fas fa-window-close"></i></button>
-                                            </div>
-
-
-                                        </div>
-                                        <div class="cart-summary">
-                                            <small>3 Item(s) selected</small>
-                                            <h5>SUBTOTAL: $2940.00</h5>
-                                        </div>
-                                        <div class="cart-btns">
-                                            <a href="#">View Cart</a>
-                                            <a href="#">Checkout  <i class="fa fa-arrow-circle-right"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
+                                <shopping-cart></shopping-cart>
                                 <!-- /Cart -->
 
                                 <!-- Menu Toogle -->
