196732415b09391af756ba1a2b0cdb7001e7c957
steffanturanjanin <stefan@turanjanin.net>
2019-02-03 23:43:40 +0100

    merge

--- public/js/app.js
+++ public/js/app.js
@@ -56568,8 +56568,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\mongo-shop-repo\mongo-shop\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\mongo-shop-repo\mongo-shop\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\wamp64\www\mongo-shop\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\wamp64\www\mongo-shop\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
