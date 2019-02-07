f655a178209e61e6cec63ec0b991841e172358ff
nemanj4t <nemanja.turanjanin@elfak.rs>
2019-02-07 16:57:35 +0100

    design

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -42,7 +42,7 @@ class ProductController extends Controller
                 break;
             }
         }
-        
+
         $path = Category::getFullPath(Category::find($product->category->id));
 
         return view('products.show', compact('path', 'product', 's1', 's2', 's3', 's4', 's5', 'attr'));
@@ -133,7 +133,7 @@ class ProductController extends Controller
     public function getGoodPrice()
     {
         $products = [];
-        $productsmongo = Product::where('grade', '>=', 2.5)->take(8)->get();
+        $productsmongo = Product::where('grade', '>=', 2.5)->orderBy('grade', 'desc')->take(8)->get();
 
         foreach ($productsmongo as $product) {
             $product->category_name = $product->category->name;
